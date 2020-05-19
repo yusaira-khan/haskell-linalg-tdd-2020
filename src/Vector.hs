ba121bf0fc3c9706392a756355ada070358f27f3
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2020-05-18 22:00:07 -0400

    add new files plus first failing test

---
+++ src/Vector.hs
@@ -0,0 +1,8 @@
+module Vector (Vector,Vector.sum, average, movingAverage, dotProduct) where
+
+type Vector = [Double]
+sum = undefined
+product = undefined
+average = undefined
+movingAverage = undefined
+dotProduct = undefined
---
+++ test/VectorSpec.hs
@@ -0,0 +1,17 @@
+ module VectorSpec (spec) where
+
+ import Vector
+ import Test.Hspec
+
+ sumTest :: (String,Vector.Vector,Double) -> Spec
+ sumTest (name,elements, result) =
+     it name $ do
+       Vector.sum(elements) `shouldBe` result
+ sumTestAll :: Spec
+ sumTestAll =
+   do
+   describe "Sum" $ do
+     sumTest ("zero",[],0)
+ spec :: Spec
+ spec = do
+   sumTestAll
