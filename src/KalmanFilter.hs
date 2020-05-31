d8de11ba25e80135bcd3f40a548a2dcf83220f0b
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2020-05-30 21:06:46 -0400

    add kalman code

--- src/KalmanFilter.hs
+++ src/KalmanFilter.hs
@@ -5,7 +5,23 @@ data KalmanFilter = KalmanFilter
  , p::Scalar -- P
  , q::Scalar -- Q
  , x::Scalar -- X
- , k::Scalar -- K
 }
 init :: KalmanFilter
-init = undefined
+init iq ir = KalmanFilter {
+  q=iq,
+  r=ir,
+  p=0,
+  x=0
+  }
+update kal ix =
+  let
+   pr = r kal
+   pq = q kal
+   pp = p kal
+   px = x kal
+   tp = pp + pq
+   tk = pp/(pp+pr)
+   nx = px + tk * (ix -px)
+   np = (1-tk)*tp
+  
+
